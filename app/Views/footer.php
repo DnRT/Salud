c9ff33389281be8c2a4ba7f3e5b8fc52dee0a1b2
DnRT <kn_ct_5290@hotmail.com>
2023-04-11 18:48:51 -0400

    Change acreditacion's image position

--- app/Views/footer.php
+++ app/Views/footer.php
@@ -6,34 +6,34 @@
 
 <div class="hidden-xs footer-dai">
     <div class="container">
-            <div class="col-md-12 col-sm-12 center" style="text-align: center">
-		<h4 style="color: white; text-align: center;">Dirección General de Postgrado y Post&iacutetulo</h4>
-		<h4 style="color: white; text-align: center;">Vicerrector&iacutea Acad&eacutemica</h4>
-                <p style="color: white; text-align: center;"><i style="color: white; text-align: left;">Blanco 951 - Piso 2 - Valparaíso, Chile</i> <br> 📞<a style="color: white; text-align: left;" href="tel:+56 32 260 3102">32 260 3102 - </a> <a style="color: white;" href="mailto:encuestas.postgrados@uv.cl">✉ encuestas.postgrados@uv.cl</a></p>
-            </div>
-
-    </div>
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="footer">
-          <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-            <div class="fuente">
-              <div class="hidden-xs hidden-sm">
-                Sistema desarrollado por la
-                <a class=" text-info fuente linkHover" href="https://analisis.uv.cl/" target="_blank">Direcci&oacuten de An&aacutelisis Institucional</a>,
-                <a class=" text-info fuente linkHover" href="https://uv.cl" target="_blank">Universidad de Valpara&iacuteso - Chile -
-                2019</a>
-              </div>
-              <div class="hidden-lg hidden-md text-center">
-                <a class=" text-info fuente linkHover" href="https://uv.cl" target="_blank">Universidad de Valpara&iacuteso - Chile -
-                2019</a>
-              </div>
-            </div>
-        </div>
-        <div class="col-lg-4 col-md-4 hidden-xs hidden-sm fuente">
+      <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 center" style="text-align: center">
+          <h4 style="color: white; text-align: center;">Dirección General de Postgrado y Post&iacutetulo <img class="hidden-xs pull-right" src="/assets/images/acreditacion_2023.png" style="height: 100px"></h4>
+          <h4 style="color: white; text-align: center;">Vicerrector&iacutea Acad&eacutemica</h4>
+          <p style="color: white; text-align: center;"><i style="color: white; text-align: left;">Blanco 951 - Piso 2 - Valparaíso, Chile</i> <br> 📞<a style="color: white; text-align: left;" href="tel:+56 32 260 3102">32 260 3102 - </a> <a style="color: white;" href="mailto:encuestas.postgrados@uv.cl">✉ encuestas.postgrados@uv.cl</a></p>
         </div>
       </div>
-
+      <div class="row"><div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="footer">
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="fuente">
+          <div class="hidden-xs hidden-sm">
+            Sistema desarrollado por la
+            <a class=" text-info fuente linkHover" href="https://analisis.uv.cl/" target="_blank">Direcci&oacuten de An&aacutelisis Institucional</a>,
+            <a class=" text-info fuente linkHover" href="https://uv.cl" target="_blank">Universidad de Valpara&iacuteso - Chile -
+            2019</a>
+          </div>
+          <div class="hidden-lg hidden-md text-center">
+            <a class=" text-info fuente linkHover" href="https://uv.cl" target="_blank">Universidad de Valpara&iacuteso - Chile -
+            2019</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-lg-4 col-md-4 hidden-xs hidden-sm fuente">
+    </div>
+  </div>
 </div>
-
+<!-- 
 <div class="visible-xs footer-dai">
     <div class="container">
         <div class="col-md-12 col-sm-12 center" style="text-align: center;margin-bottom: 25px;">
@@ -63,7 +63,7 @@
         </div>
       </div>
     </div>
-</di  v>
+</div> -->
 </body>
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-104079464-7"></script>
